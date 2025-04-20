4047b16e58760d57ed59cfc69fde6df854c8a24f
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-04-20 18:31:08 +0200

    header sect + responsive  1

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,4 +1,45 @@
+import React from "react";
+import "./Homepage.css";
+
+//immagini
+import homeImg from "../../Assets/Home/Isolotto-Pirgo-6.jpg";
 const Homepage = () => {
-  return <h1>isolotto del pirgo</h1>;
+  return (
+    <>
+      {/* HEADER SECTION */}
+      <div className="header-section">
+        <img src={homeImg} alt="" />
+        <div className="title">
+          <h1>ISOLOTTO DEL PIRGO</h1>
+          <div className="scrolltxt-container">
+            <h3>Prossima Apertura Maggio 2025</h3>
+          </div>
+          <button>PRENOTA UN TAVOLO</button>
+        </div>
+        <div className="arrow-down-sect">
+          <div id="scrollDown">
+            <div className="scroll-down-indicator">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12 5V19M12 19L5 12M12 19L19 12"
+                  stroke="#ffffff"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/**/}
+    </>
+  );
 };
 export default Homepage;
