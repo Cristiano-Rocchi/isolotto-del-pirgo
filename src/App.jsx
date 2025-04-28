c79ceb750f967288546f15cfc70cbef325f21ce3
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-04-28 12:32:15 +0200

    navbar 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./Components/Homepage/Homepage";
 import Menu from "./Components/Menu/Menu";
 import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/menu" element={<Menu />} />
